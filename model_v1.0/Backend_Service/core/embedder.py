4414043f2e0bff5c504086c0ac2b828039537f16
Aryan <aryan.mishra20786@gmail.com>
2025-05-21 14:51:49 +0530

    Delete model_v1.0 directory
    Signed-off-by: Aryan <aryan.mishra20786@gmail.com>

--- model_v1.0/Backend_Service/core/embedder.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from typing import List
-from sentence_transformers import SentenceTransformer
-
-model = SentenceTransformer('all-MiniLM-L6-v2')
-
-def embed_chunks(chunks: List[str]) -> np.ndarray:
-    return model.encode(chunks, show_progress_bar=True)
